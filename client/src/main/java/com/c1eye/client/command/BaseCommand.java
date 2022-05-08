90325c345265e15e395688f597341934e88e8f63
c1eye <c1eyemmj@gmail.com>
2022-05-08 21:26:04 +0800

    init

---
+++ client/src/main/java/com/c1eye/client/command/BaseCommand.java
@@ -0,0 +1,15 @@
+package com.c1eye.client.command;
+
+/**
+ * @author c1eye
+ * time 2022/5/8 19:50
+ */
+public interface BaseCommand {
+
+    /**
+     * 执行
+     */
+    void execute();
+
+
+}
---
+++ client/src/main/java/com/c1eye/client/command/LoginConsoleCommand.java
@@ -0,0 +1,13 @@
+package com.c1eye.client.command;
+
+/**
+ * @author c1eye
+ * time 2022/5/8 20:46
+ */
+public class LoginConsoleCommand implements BaseCommand{
+
+    @Override
+    public void execute() {
+
+    }
+}
